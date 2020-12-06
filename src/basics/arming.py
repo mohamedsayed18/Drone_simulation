c3eb9a34d9380cf3c7559b9b7cfaf832d2e384b0
mohamedsayed18 <mohamed95.a.s@gmail.com>
2020-12-06 19:11:36 +0300

    add comments to arming

--- src/basics/arming.py
+++ src/basics/arming.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 """
-change the mode of the flight and set arming on
+This script detect an AR marker and land on it. the sequence is as follow
+1- Set the offboard mode
+2- Arming on
+3- fly to the target position
+4- detect the marker and land on it
+
+Notes:
 Before entering Offboard mode, you must have already started streaming setpoints.
  Otherwise the mode switch will be rejected.
 """
@@ -9,20 +15,22 @@ Before entering Offboard mode, you must have already started streaming setpoints
 import rospy
 from mavros_msgs.srv import SetMode
 from mavros_msgs.srv import CommandBool
+from mavros_msgs.srv import CommandTOL
 from geometry_msgs.msg import PoseStamped
 from ar_track_alvar_msgs.msg import AlvarMarkers
-from mavros_msgs.msg import PositionTarget
-from nav_msgs.msg import Odometry
-from mavros_msgs.srv import CommandTOL
 
+# Global variables for the drone position, marker_position
 glo_pos_z = 0
 glo_pos_x = 0
 glo_pos_y = 0
 marker_x = 0
 marker_y = 0
-detected = 0
+detected = 0	#	true if the marker is detected 
 
 def landing(data):
+	"""
+	detect the marker and set the new target pose
+	"""
 	global target_pose
 	global marker_sub
 	global glo_pos_x
@@ -30,41 +38,26 @@ def landing(data):
 	global marker_x
 	global marker_y
 	global detected
-	#print("landing callback")
-	#rospy.loginfo(len(data.markers))
-	#print (data)
+	
 	try:
 		if data.markers[0].id == 50:
-			### to get the position of the tag
+			# to get the position of the tag
 			print("x-position ", data.markers[0].pose.pose.position.x, "y-postion", data.markers[0].pose.pose.position.y)
 			print("global-x ",glo_pos_x, "global-y", glo_pos_y )
 			marker_x = data.markers[0].pose.pose.position.x
 			marker_y = data.markers[0].pose.pose.position.y
+			# set the new target pose
 			target_pose.pose.position.x = glo_pos_x - marker_y-0.2
 			target_pose.pose.position.y = glo_pos_y - marker_x-0.2
-			detected = 1
-			#marker_sub.unregister()
-			
+			detected = 1	
 	except:
 		pass
 		
-"""
-		while(glo_pos_x <= target_pose.pose.position.x and glo_pos_y <= target_pose.pose.position.y):
-			pub.publish(target_pose)
-			rate.sleep()
-
-		print("ended the while")
-		marker_sub.unregister()
-		rospy.wait_for_service('/mavros/cmd/land')
-   		try:
-			landService = rospy.ServiceProxy('/mavros/cmd/land', CommandTOL)
-			#http://wiki.ros.org/mavros/CustomModes for custom modes
-			isLanding = landService(altitude = 0, latitude = 0.8, longitude = 0.8, min_pitch = 0, yaw = 0)
-		except rospy.ServiceException, e:
-			print "service land call failed: %s. The vehicle cannot land "%e
-			rospy.loginfo("detected")		
-"""
 def glo_pos(data):
+	"""
+	callback for the local position
+	it set the drone position to the global values
+	"""
 	global glo_pos_z
 	global glo_pos_x
 	global glo_pos_y
@@ -79,53 +72,48 @@ if __name__ == '__main__':
 	target_pose.pose.position.x = 1.5
 	target_pose.pose.position.y = 0
 	target_pose.pose.position.z = 2.5
-	
-	"""
-	target_pose.pose.position.x = .3
-	target_pose.pose.position.y = 0.3
-	target_pose.pose.position.z = 2
-	"""
-	pub = rospy.Publisher('mavros/setpoint_position/local', PoseStamped, queue_size=10)  # publisher
-	#pub = rospy.Publisher('/mavros/setpoint_raw/local', PositionTarget, queue_size=10)  # publisher
-	#marker_sub	= rospy.Subscriber("/ar_pose_marker", AlvarMarkers, landing) #subscriber
-	suby = rospy.Subscriber("/mavros/local_position/pose", PoseStamped, glo_pos) #subscriber
-	rospy.init_node('arming', anonymous=True)    # regulator node
-	rate = rospy.Rate(100) # 10hz
 
+	# publisher, the target pose of the drone
+	pub = rospy.Publisher('mavros/setpoint_position/local', PoseStamped, queue_size=10)
+	# subscriber to the pose of the drone
+	suby = rospy.Subscriber("/mavros/local_position/pose", PoseStamped, glo_pos)
+	rospy.init_node('arming', anonymous=True)    # node initialization
+	rate = rospy.Rate(100) # 100hz
+
+	# Publish some set points before entering the offboard mode
 	for i in range(100):
 		pub.publish(target_pose)
 		rate.sleep()
-	print("done setting")
     
 	# OFF board mode
 	rospy.wait_for_service('/mavros/set_mode')  # block until service is available 
 	mode_srv = rospy.ServiceProxy("/mavros/set_mode", SetMode)
 	response = mode_srv.call(custom_mode="OFFBOARD")
-	print(response)
+	print("Offboard mode ", response)
 
 	# Arming ON
 	rospy.wait_for_service("mavros/cmd/arming")
 	arming_srv = rospy.ServiceProxy("mavros/cmd/arming", CommandBool)
 	arm_response = arming_srv.call(True)
-	print(arm_response)
+	print("Arming ", arm_response.success)
 	flagy = 0
 	
 	while not rospy.is_shutdown():
-		# publishing 
+		# publishing target position
 		pub.publish(target_pose)
 
-		if (glo_pos_z >= 1.9 and flagy==0):
-			marker_sub	= rospy.Subscriber("/ar_pose_marker", AlvarMarkers, landing) #subscriber
-			#suby.unregister()
+		if (glo_pos_z >= 2.3 and flagy==0):	# if drone reached the target
+			#	Subscriber to the marker
+			marker_sub	= rospy.Subscriber("/ar_pose_marker", AlvarMarkers, landing)
 			print("subscriber created")
 			flagy=1
-			#marker_sub	= rospy.Subscriber("/ar_pose_marker", AlvarMarkers, landing) #subscriber
+			
+		# if marker is found and the drone is above it, start landing
 		if (detected == 1 and -0.23<marker_x<-0.2 and -0.23<marker_y<-0.2):
-			marker_sub.unregister()
+			marker_sub.unregister()	# no longer need to know the position of the marker
 			rospy.wait_for_service('/mavros/cmd/land')
 			try:
 				landService = rospy.ServiceProxy('/mavros/cmd/land', CommandTOL)
-				#http://wiki.ros.org/mavros/CustomModes for custom modes
-				isLanding = landService(altitude = 0, latitude = 0.8, longitude = 0.8, min_pitch = 0, yaw = 0)
+				isLanding = landService(altitude = 0, latitude = 0, longitude = 0, min_pitch = 0, yaw = 0)
 			except rospy.ServiceException, e:
-				print "service land call failed: %s. The vehicle cannot land "%e
+				print "service land call failed"
